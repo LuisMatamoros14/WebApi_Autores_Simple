79a9126cb7e49dbc7a73798e36695426e4df9eef
LuisMatamoros14 <luismatamoroszarate@gmail.com>
2023-02-28 17:51:34 -0600

    Endpoint registrar usuario y devolver token

---
+++ WebApi_Autores/Controllers/CuentasController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApi_Autores.DTOs;
+
+namespace WebApi_Autores.Controllers
+{
+    [ApiController]
+    [Route("api/cuentas")]
+    public class CuentasController: ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly ApplicationDbContext context;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IConfiguration configuration;
+
+        public CuentasController(IMapper mapper, ApplicationDbContext context, UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            this.mapper = mapper;
+            this.context = context;
+            this.userManager = userManager;
+            this.configuration = configuration;
+        }
+
+        [HttpPost("registrar")]
+        public async Task<ActionResult<RespuestaAutenticacion>> Registrar(CredencialesUsuario credencialesUsuario)
+        {
+            var usuario = new IdentityUser{ UserName= credencialesUsuario.Email, Email=credencialesUsuario.Email};
+            var resultado = await userManager.CreateAsync(usuario, credencialesUsuario.Password);
+
+            if (resultado.Succeeded)
+            {
+                return ConstruirToken(credencialesUsuario);
+            }
+            else
+            {
+                return BadRequest(resultado.Errors);
+            }
+        }
+
+        private RespuestaAutenticacion ConstruirToken(CredencialesUsuario credencialesUsuario)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("email", credencialesUsuario.Email)
+            };
+
+            var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["token"]));
+
+            var creds = new SigningCredentials(llave, SecurityAlgorithms.HmacSha256);
+            var expiracion = DateTime.UtcNow.AddMinutes(60);
+            var securityToken = new JwtSecurityToken(issuer: null, audience: null, claims: claims, expires: expiracion, signingCredentials: creds);
+
+            return new RespuestaAutenticacion()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(securityToken),
+                Expiracion = expiracion
+            };
+        }
+    }
+}
---
+++ WebApi_Autores/DTOs/CredencialesUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi_Autores.DTOs
+{
+    public class CredencialesUsuario
+    {
+        [Required(ErrorMessage = "El campo {0} es necesario")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
